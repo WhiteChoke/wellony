76c49f2b9ef780f83d649e542bf5b9c4ab0044ca
WhiteChoke <denisduchenkoo@gmail.com>
2026-05-29 14:54:39 +0900

    initial commit

---
+++ wellony-api/src/test/java/git/whitechoke/wellony/WellonyApplicationTests.java
@@ -0,0 +1,13 @@
+package git.whitechoke.wellony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
