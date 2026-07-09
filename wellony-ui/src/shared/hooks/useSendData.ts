4911b88630bcd71d06b2cedb0ddb6501d8172e86
WhiteChoke <denisduchenkoo@gmail.com>
2026-07-09 21:57:43 +0900

    implemented send data hook

--- wellony-ui/src/shared/hooks/useSendData.ts
+++ wellony-ui/src/shared/hooks/useSendData.ts
@@ -1,1 +1,33 @@
-export default function useSendData<TResponse, >()
+import {useAuthContext} from "../contexts/authContext.ts";
+import {useState} from "react";
+import {apiClient} from "../api/Client.ts";
+
+export default function useSendData<TResponse, TBody = Record<string, never>>(url: string, data: TBody) {
+    const {auth} = useAuthContext();
+    const [isLoading, setIsLoading ] = useState(false);
+    const [error, setError] = useState(false);
+
+    async function sendRequest() {
+        setIsLoading(true);
+
+        try{
+            const response = await apiClient.post<TResponse>(url, data, {
+                headers: {
+                    Authorization: `Bearer ${auth.token}`
+                }
+            })
+
+            return response.data;
+
+        } catch (e) {
+            console.error(e);
+            setError(true);
+
+            return null;
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    return {sendRequest, isLoading, error};
+}
